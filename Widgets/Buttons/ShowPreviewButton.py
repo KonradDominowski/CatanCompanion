2b52c49e41349b6bd6b1ff8e1b83f36c0404b021
KonradDominowski <konrad.dominowski@gmail.com>
2025-05-14 11:49:29 +0200

    Added code for camera to work on Pi

--- Widgets/Buttons/ShowPreviewButton.py
+++ Widgets/Buttons/ShowPreviewButton.py
@@ -1,7 +1,8 @@
 from PySide6.QtCore import QSize
 from PySide6.QtWidgets import QPushButton
+from picamera2 import Picamera2
 import cv2
-
+import time
 
 class ShowPreviewButton(QPushButton):
     def __init__(self, text: str):
@@ -12,14 +13,34 @@ class ShowPreviewButton(QPushButton):
         self.clicked.connect(self.show_preview)
 
     def show_preview(self):
-        capture = cv2.VideoCapture(0)
+        picam2 = Picamera2()
+        picam2.preview_configuration.main.size = (640, 480)
+        picam2.preview_configuration.main.format = "RGB888"
+        picam2.configure("preview")
+
+        picam2.start()
+        # ~ time.sleep(1)  # chwilka na uruchomienie kamery
 
         while True:
-            ret, frame = capture.read()
+            frame = picam2.capture_array()
 
-            cv2.imshow('frame', frame)
+            cv2.imshow("Preview", frame)
             if cv2.waitKey(1) == ord('q'):
                 break
 
-        capture.release()
         cv2.destroyAllWindows()
+        picam2.stop()
+    
+    
+    # ~ def show_preview(self):
+                # ~ capture = cv2.VideoCapture(0)
+
+        # ~ while True:
+            # ~ ret, frame = capture.read()
+
+            # ~ cv2.imshow('frame', frame)
+            # ~ if cv2.waitKey(1) == ord('q'):
+                # ~ break
+
+        # ~ capture.release()
+        # ~ cv2.destroyAllWindows()
